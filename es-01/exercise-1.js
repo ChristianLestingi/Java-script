6a5b904f108ef9ba9d941f3b031034f5c003a3ce
Christian <christianlestingi33@gmail.com>
2022-08-03 21:38:42 +0200

    esercizi javascriopt

---
+++ es-01/exercise-1.js
@@ -0,0 +1,18 @@
+ 
+let personName = 'Paul';
+ 
+ function canPlay() {
+  if (true) {
+   
+    personName += ' plays football';
+    console.log(personName);
+  
+  }
+
+  // Argh! personName is not defined//
+
+  
+
+}
+
+canPlay();
